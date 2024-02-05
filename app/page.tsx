137e0de0007407ef7b8f1557ab406e1752b9564e
yogjin <youngjin988@gmail.com>
2024-02-05 17:54:13 +0900

    chore: 하루 로고 추가 및 로그인 페이지에 적용

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 'use client';
-import { Button, Divider, Input } from 'antd';
+import { Button, Divider, Flex, Input } from 'antd';
 import { buttonContainer, container, inputContainer } from './page.css';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className={container}>
+      <Flex justify="center">
+        <Image src="/logo.svg" alt="logo" width={125} height={125} />
+      </Flex>
       <section className={inputContainer}>
         <Input placeholder="아이디" size="large" />
         <Input.Password placeholder="패스워드" size="large" />
